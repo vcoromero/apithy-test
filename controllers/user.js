d2c96dc86e7077b137b58360cdf13c6a7c8a5031
Víctor Jesús Romero Pérez <vcoromero@gmail.com>
2020-11-03 22:17:30 -0600

    insertando en la tabla users

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const XLSX = require('xlsx');
 
 //metodos de prueba
 function home(request, response) {
-  response
+  return response
     .status(200)
     .send({ message: "Api de user" });
 }
@@ -18,7 +18,7 @@ async function getUsers(req, res) {
     paginate: 50,
   }
   const { docs, pages, total } = await User.paginate(options)
-  res.status(200).send({ users: docs, actual_page: Number(page), total_pages: pages, total_users: total })
+  return res.status(200).send({ users: docs, actual_page: Number(page), total_pages: pages, total_users: total })
 }
 
 
@@ -31,67 +31,145 @@ async function getUser(req, res) {
       id: user_id
     }
   })
-  res.status(200).send(users)
+  return res.status(200).send(users)
 }
 
-async function importFromExcel(req, res) {
 
+function insertUser(data) {
+  //sub divido el array para hacerlos mas pequenos e insertar por lotes
+  let sub_array = []
+  let new_array = []
+  for (let i = 0; i < data.data_to_insert.length; i += 1000) {
+    sub_array = data.data_to_insert.slice(i, i + 1000)
+    new_array.push(sub_array)
+  }
+  new_array.forEach(element => {
+    //inserto por lotes
+    User.bulkCreate(element).then(() => {
+      return true
+    }).catch(() => {
+      return false
+    })
+  });
+}
+async function validateData(data) {
+  //variables para mostrar en el response
+  let aux_validador = {}
+  const validador = []
+  let aux_success = {}
+  const success = []
+  let aux_data_to_insert = {}
+  let data_to_insert = []
+
+  //obteniendo todos los roles
   const exist_roles = []
   const roles = await Role.findAll()
   for (role of roles) {
-    exist_roles.push(role.name)
+    exist_roles.push({ id: role.id, code: role.code })
   }
 
+  for (const item of data) {
 
-  if (req.files) {
-    const file_path = req.files.excel.path;
-    const workbook = XLSX.readFile(file_path);
-    const sheet = workbook.SheetNames[0];
-    const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+    //validando los datos
+    aux_validador = {}
+    aux_success = {}
+    aux_data_to_insert = {}
 
-    const validador = {}
-    for (const item of dataExcel) {
-      if (exist_roles.includes(item.code)) {
-        //pasa
-      } else {
-        // no pasa
-        validador.role =
-        {
-          value: item.code,
-          validation: ''
-        }
-
+    //nombre mas de 50
+    if (item.name.length > 50) {
+      //no pasa
+      aux_validador.role =
+      {
+        value: item.name,
+        validation: 'El nombre es mayor a 5 caracteres'
       }
-      //nombre mas de 50
-      if (item.name.length > 50) {
-        //no pasa
-      } else {
-        validador.name =
-        {
-          value: item.name,
-          validation: ''
-        }
+    } else {
+      //si pasa
+      aux_data_to_insert.name = item.name
+      aux_success.name = item.name
+      aux_validador.name =
+      {
+        value: item.name,
+        validation: ''
       }
-      const exist_email = await User.findAll({
-        where: {
-          email:
-            item.email
-        }
-      })
-      if (Object.keys(exist_email).length) {
-        //console.log(exist_email[0].email)
-      } else {
-        validador.email =
-        {
-          value: item.email,
-          validation: ''
-        }
+    }
+    //email
+    const exist_email = await User.findAll({
+      where: {
+        email:
+          item.email
+      }
+    })
+    if (Object.keys(exist_email).length) {
+      aux_validador.role =
+      {
+        value: item.email,
+        validation: 'El email ya existe en la base de datos'
+      }
+    } else {
+      aux_data_to_insert.email = item.email
+      aux_success.email = item.email
+      aux_validador.email =
+      {
+        value: item.email,
+        validation: ''
+      }
+    }
+    //role
+    const find_role = exist_roles.find(role => role.code === item.code)
+    if (find_role) {
+      //pasa
+      aux_data_to_insert.roleId = find_role.id
+      aux_success.code = item.code
+      aux_validador.role =
+      {
+        value: item.code,
+        validation: ''
+      }
+    } else {
+      // no pasa
+      aux_validador.role =
+      {
+        value: item.code,
+        validation: 'No existe el rol'
       }
 
     }
-    console.log(validador)
-  }
 
+
+    success.push(aux_success)
+    validador.push(aux_validador)
+    data_to_insert.push(aux_data_to_insert)
+
+  }
+  return { success, validador, data_to_insert }
+
+}
+async function importFromExcel(req, res) {
+  //leyendo el archivo excel
+  const file_path = req.files.excel.path;
+  const workbook = XLSX.readFile(file_path);
+  const sheet = workbook.SheetNames[0];
+  const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+
+  //metodo para validar datos del excel
+  const validatedData = await validateData(dataExcel)
+  //aqui hago la insercion de los datos a la bd
+  const insert = insertUser(validatedData)
+  if (insert) {
+    return res.status(200).send({
+      error: '',
+      code: 200,
+      hint: 'success',
+      message: 'success',
+      data: [
+        {
+          success: validatedData.success,
+          validation: validatedData.validador
+        }
+      ]
+    })
+  }
 
 }
 
